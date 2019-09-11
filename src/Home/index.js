a2d3988519da19ba3df1d16fd2953ae6bf2b145d
Erin Johnson <erinjohnson47@gmail.com>
2019-09-11 09:21:14 -0700

    add home page

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Home = () => {
         return (
-            <div>
-            This is the home page.
+            <div id='homePage'>
+                <h3>Welcome to Pantry Pal!</h3>
+                <h5>A place where you can store your food inventory and keep track of what items you have in your pantry, fridge, and freezer!  <u>Please register as a new user to get started.</u></h5>
             </div>
     )
 }
